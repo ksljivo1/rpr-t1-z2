e6b9c55eb19c9d121d77b2c07ae221b7d1f6c3cf
ksljivo1 <ksljivo1@etf.unsa.ba>
2020-10-31 17:05:41 +0100

    Funkcija sumaCifara implementirana u mainu

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,8 +1,16 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Unesite prirodan broj n: ");
+        Scanner ulaz = new Scanner(System.in);
+        int n = ulaz.nextInt();
+        System.out.println("Brojevi djeljivi sumom svojih cifara su: ");
+        for(int i = 1; i <= n; i++) {
+            if(i % Suma.sumaCifara(i) == 0) System.out.println(i);
+        }
     }
 }
